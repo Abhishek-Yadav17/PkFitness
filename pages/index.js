cd408ad75eabdad85e31655708d7bd043198e7a8
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-07-18 18:24:20 +0530

    added keywords

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ export default function Home() {
     <>
       <Head>
         <title>PK Fitness & Nutrition</title>
+        <meta name="description" content="Your app for users in the United States, Canada, European countries, Australia, and India." />
+        <meta name="keywords" content="United States, Canada, Europe, Australia, India" />
       </Head>
       <main data-scroll-container style={{ height: '100%', overflow: 'hidden' }}>
         <div className="animate">
