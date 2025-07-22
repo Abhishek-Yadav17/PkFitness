1a7bda686a8341d3b2ecf46c44acd7706c090e90
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-07-22 13:52:58 +0530

    added viewport

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet"></link>
       <link rel="icon" href="/pkfitnesslogo.jpg" />
       <body>
