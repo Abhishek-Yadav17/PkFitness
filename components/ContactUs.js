35e0536d29c7148b5a3fb24bd76f5f5962fcee77
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-07-22 14:18:53 +0530

    added backend

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -22,7 +22,7 @@ export default function ContactUs() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/send', {
+      const res = await fetch('https://pkbackend-cb1x.onrender.com/send', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
